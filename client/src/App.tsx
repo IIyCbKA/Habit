06807c680118e3acfc19689de33d1dd9b787a163
IIyCbKA <alex.chernobrovenko2005@yandex.ru>
2025-04-08 02:27:31 +0300

    Initial client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+import AppRoutes from "./routes/AppRoutes";
+
+export default function App(): React.ReactElement {
+  return (
+    <>
+      <AppRoutes />
+    </>
+  );
+}
