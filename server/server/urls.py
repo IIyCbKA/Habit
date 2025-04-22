db7587045a9a78e82d277512134498ae487788e9
Denis Lylov <denislylov2004@mail.ru>
2025-04-22 23:13:56 +0300

    CORS added, changed urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from habit_server import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login),
-    path('signup/', views.signup),
-    path('test_token/', views.test_token),
+    path('auth/', include("habit_server.urls")),
 ]
